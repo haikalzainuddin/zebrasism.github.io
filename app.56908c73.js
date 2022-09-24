fed2989ec59b6525b5256db5910d6724b70ea973
HaikalZai Desktop <haikalzainuddin@gmail.com>
2022-09-24 11:10:08 +0800

    update og title

--- app.56908c73.js
+++ app.56908c73.js
@@ -52206,7 +52206,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53526" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63980" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
