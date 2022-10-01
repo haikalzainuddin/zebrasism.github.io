6b5e0b83ddcf50a6a82c740e2a3734b1fceaf67f
Haikal Zainuddin <haikalzainuddin@gmail.com>
2022-10-01 14:54:22 +0800

    update rsvp date, add new page, guest page update

--- app.56908c73.js
+++ app.56908c73.js
@@ -52206,7 +52206,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62307" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55953" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
